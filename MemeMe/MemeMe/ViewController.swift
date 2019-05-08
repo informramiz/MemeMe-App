1addf26c5c2177a641c779eb08143aa624940f94
Ramiz Raja <informramiz@gmail.com>
2019-05-09 01:04:35 +0500

    feat: Show image picker

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-    private var count = 0
-    @IBOutlet var counterLabel: UILabel!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
     
-    //MARK: increments the counter and updates the label
-    @IBAction private func incrementCount() {
-        count += 1
-        self.counterLabel.text = "\(self.count)"
+    @IBAction private func experiment() {
+        let controller = UIImagePickerController()
+        self.present(controller, animated: true, completion: nil)
     }
 }
 
