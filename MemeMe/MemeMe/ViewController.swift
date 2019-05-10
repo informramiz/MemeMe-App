867aacec198310efb7fcaf9127789e643feb8674
Ramiz Raja <informramiz@gmail.com>
2019-05-10 10:15:08 +0500

    fix: Fixing forground text color not working

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -17,8 +17,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     
     //apply default text attributes for our text fields
     private let memeTextAttributes: [NSAttributedString.Key: Any] = [
-        NSAttributedString.Key.strokeColor: UIColor.white,
-        NSAttributedString.Key.strokeWidth: 3,
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.strokeWidth: -3,
         NSAttributedString.Key.foregroundColor: UIColor.white,
         NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
     ]
