d5bb874ed74cfa06f2a4672078393cbeb43eb11b
Ramiz Raja <informramiz@gmail.com>
2019-05-10 08:57:39 +0500

    feat: Apply text attributes

--- MemeMe/MemeMe/ViewController.swift
+++ MemeMe/MemeMe/ViewController.swift
@@ -15,8 +15,19 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
+    //apply default text attributes for our text fields
+    private let memeTextAttributes: [NSAttributedString.Key: Any] = [
+        NSAttributedString.Key.strokeColor: UIColor.black,
+        NSAttributedString.Key.strokeWidth: 2,
+        NSAttributedString.Key.foregroundColor: UIColor.white,
+        NSAttributedString.Key.font: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        //apply our custom define default text attributes
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
         // Do any additional setup after loading the view.
         topTextField.delegate = self
         bottomTextField.delegate = self
