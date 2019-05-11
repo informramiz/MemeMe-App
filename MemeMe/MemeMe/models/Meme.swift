f3c36b015b6e238bab384299924844b34dd12efc
Ramiz Raja <informramiz@gmail.com>
2019-05-12 03:51:07 +0500

    feat: Write Meme struct

---
+++ MemeMe/MemeMe/models/Meme.swift
@@ -0,0 +1,15 @@
+//
+//  Meme.swift
+//  MemeMe
+//
+//  Created by Apple on 12/05/2019.
+//  Copyright © 2019 RR Inc. All rights reserved.
+//
+import UIKit
+
+struct Meme {
+    let topText: String
+    let bottomText: String
+    let originalImage: UIImage
+    let memedImage: UIImage
+}
